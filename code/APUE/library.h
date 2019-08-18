8fdfa1d4461b151c8d8059990f1e64e63b5c888e
ezio <eziosudo@gmail.com>
2019-08-18 14:06:48 +0800

    现代操作系统 进程与线程 课后习题

--- code/APUE/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef APUE_LIBRARY_H
-#define APUE_LIBRARY_H
-
-void hello(void);
-
-#endif
--- code/APUE/main.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by ezio on 2019-08-17.
-//
-#include <stdio.h>
-
-int main(){
-    printf("test");
-}
